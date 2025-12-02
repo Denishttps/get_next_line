be816a66f3d69daf783049403de4a519a59b7cf8
Denis <bobrovdenis2007@gmail.com>
2025-12-02 13:09:12 +0100

    add header file for get_next_line functionality

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbobrov <dbobrov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 15:48:04 by dbobrov           #+#    #+#             */
+/*   Updated: 2025/12/02 13:08:53 by dbobrov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <stdint.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+#endif
+
+char	*get_next_line(int fd);
+
+
+#endif
